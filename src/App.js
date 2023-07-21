3c215f8b99323689ff76c72f32c7565293c15464
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-07-21 12:08:20 +0800

    migrated userContext to redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch } from 'react-redux';
+
 import { Routes, Route } from 'react-router-dom';
+
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangedListener,
+} from "./utils/firebase/firebase.utils";
+import { setCurrentUser } from './store/user/user.action';
+
+
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
@@ -6,7 +17,24 @@ import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
+
+
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      console.log(user);
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+  }, [dispatch]); // only to get rid of warnings, in fact dispatch will not change
+
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
