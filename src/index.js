829ebb64e5b7f3fd4e4ebbc9e2bf95d85c836d8c
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-07-21 10:45:09 +0800

    set up redux

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <React.StrictMode>    
     <BrowserRouter>
       <UserProvider>
         <CategoriesProvider>
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
-
 import "./shop.styles.scss";
 
 const Shop = () => {
@@ -10,7 +9,6 @@ const Shop = () => {
     <Routes>
       <Route index element={<CategoriesPreview />} />
       <Route path=":category" element={<Category />} />
-
     </Routes>
   );
 };
