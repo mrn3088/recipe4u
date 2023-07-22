1991e244bd486ccde5c7d507c44d05575675bea2
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-07-23 01:01:22 +0800

    updated userSession to saga

---
+++ src/store/user/user.saga.js
@@ -0,0 +1,41 @@
+import { takeLatest, put, all, call } from "redux-saga/effects";
+
+import { USER_ACTION_TYPES } from "./user.types";
+
+import { signInSuccess, signInFailed } from "./user.action";
+
+import {
+  createUserDocumentFromAuth,
+  getCurrentUser,
+} from "../../utils/firebase/firebase.utils";
+
+export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
+  try {
+    const userSnapShot = yield call(
+      createUserDocumentFromAuth,
+      userAuth,
+      additionalDetails
+    );
+    yield put(signInSuccess({ id: userSnapShot.id, ...userSnapShot.data() }));
+  } catch (error) {
+    yield put(signInFailed(error));
+  }
+}
+
+export function* isUserAuthenticated() {
+  try {
+    const userAuth = yield call(getCurrentUser);
+    if (!userAuth) return;
+    yield call(getSnapshotFromUserAuth, userAuth);
+  } catch (error) {
+    yield put(signInFailed(error));
+  }
+}
+
+export function* onCheckUserSession() {
+  yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
+}
+
+export function* userSagas() {
+  yield all([call(onCheckUserSession)]);
+}
