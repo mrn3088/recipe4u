77c36f920b6423943d9d3c716e333f412f9b6fed
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-07-22 22:29:46 +0800

    migrated thunk to saga

--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,30 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
 
-import { rootReducer } from './root-reducer';
+import { rootSaga } from "./root-saga";
+
+import { rootReducer } from "./root-reducer";
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-  whitelist: ['cart'],
+  whitelist: ["cart"],
 };
+
+const sagaMiddleware = createSagaMiddleware();
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
-  process.env.NODE_ENV !== 'production' && logger,
-  thunk,
+  process.env.NODE_ENV !== "production" && logger,
+  sagaMiddleware,
 ].filter(Boolean);
 
 const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
+  (process.env.NODE_ENV !== "production" &&
     window &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
@@ -32,5 +36,7 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
