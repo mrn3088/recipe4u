c7c06e7fa9570f455c65a5c6e73b33c5ba19670a
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-07-22 21:52:30 +0800

    fixed bug in spinner

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,33 +1,40 @@
-import { useState, useEffect, Fragment } from "react";
-import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import { useState, useEffect, Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-import { Title, CategoryContainer } from "./category.styles";
-import ProductCard from "../../components/product-card/product-card.component";
+import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
+
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading,
+} from '../../store/categories/category.selector';
+
+import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
-  console.log("render/re-rendering category component");
 
   useEffect(() => {
-    console.log("useEffect in category component");
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
 
   return (
     <Fragment>
       <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map(
-            (
-              product // here, add a safeguard
-            ) => <ProductCard key={product.id} product={product} />
-          )}
-      </CategoryContainer>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
     </Fragment>
   );
 };
