dd3f88ee7c2ddf34fa57a011f8f02ebc1ff3b27c
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-07-29 02:06:15 +0800

    feat(netlify-functions): add a new serverless function
    This commit adds a new serverless function for handling stripe payment.

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
-  try {
+  try { 
     const { amount } = JSON.parse(event.body);
 
     const paymentIntent = await stripe.paymentIntents.create({
