7fe8b118e819462627bcb1c0769e6b00f270c300
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-07-28 16:04:30 +0800

    refactor: migrate project to typescript
    migrated root reducer, middlewares to typescript

--- src/store/user/user.selector.ts
+++ src/store/user/user.selector.ts
@@ -1,8 +1,10 @@
 import { UserState } from "./user.reducer";
 
+import { RootState } from "../store";
+
 import { createSelector } from "reselect";
- 
-export const selectUserReducer = (state): UserState => state.user;
+
+export const selectUserReducer = (state: RootState): UserState => state.user;
 
 export const selectCurrentUser = createSelector(
   selectUserReducer,
