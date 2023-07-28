62d10ed5f2a414042d3be68b4667d6e99c28078e
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-07-28 15:35:44 +0800

    refactor: migrate project to typescript
    migrated user to typescript

--- src/store/user/user.selector.ts
+++ src/store/user/user.selector.ts
@@ -1,7 +1,7 @@
 import { UserState } from "./user.reducer";
 
 import { createSelector } from "reselect";
-
+ 
 export const selectUserReducer = (state): UserState => state.user;
 
 export const selectCurrentUser = createSelector(
