3f68335da70bdfffb8e821ee792ba2741ca8a7cc
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-07-29 01:17:37 +0800

    refactor: migrate project to typescript
    migrated cart-dropdown, cart-item, category-preview, stripe util to typescript

---
+++ src/components/category-preview/category-preview.component.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+import { CategoryItem } from "../../store/categories/category.types";
+import ProductCard from "../product-card/product-card.component";
+
+import "./category-preview.styles";
+import {
+  CategoryPreviewContainer,
+  Preview,
+  Title,
+} from "./category-preview.styles";
+
+type CategoryPreviewProps = {
+  title: string;
+  products: CategoryItem[];
+};
+
+const CategoryPreview: FC<CategoryPreviewProps> = ({ title, products }) => {
+  return (
+    <CategoryPreviewContainer>
+      <h2>
+        <Title to={title}>{title.toUpperCase()}</Title>
+      </h2>
+      <Preview>
+        {products
+          .filter((_, idx) => idx < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </Preview>
+    </CategoryPreviewContainer>
+  );
+};
+
+export default CategoryPreview;
--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ import { store, persistor } from "./store/store";
 
 import "./index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") || document.createElement("div"));
 
 root.render(
   <React.StrictMode>
